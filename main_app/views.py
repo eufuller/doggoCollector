952f09a0736d1fe4763e70f723a798425fd35331
Essence Fuller <105015686+eufuller@users.noreply.github.com>
2022-07-05 11:30:48 -0700

    render working, templates, working, static stylesheets working

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hellor</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
+    # return render(request, 'about.html')
+
+def dogs_index(request):
+    return HttpResponse('<h1>Doggos</h1>')
+    # return render(request, 'doggoIndex.html')
